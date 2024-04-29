5f29fe1b2602452a4c66cd08d81703c5b0e2c2a3
Angelmmc <ammcardenas@hotmail.com>
2024-04-29 02:23:36 -0600

    conditions finished

--- assets/js/script-2.js
+++ assets/js/script-2.js
@@ -20,111 +20,117 @@ document.addEventListener('DOMContentLoaded', function () {
 
                 console.log('Orden identificada:', result);
 
-                if (result.includes("nugget")){
+                if (result.includes("nugget")) {
 
                     console.log("entro");
 
 
-                switch (true) {
-                    // Cambia el tamaño del texto al 5 de bootstrap al decir "tamaño 5"
-                    case result.includes("enciende") && result.includes("luz") && result.includes("cuarto"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Encender luz del cuarto");
-                        putJson(1,1,0,0,0,0,0,0);
-                        postJson("Encender luz del cuarto");
-                        break;
+                    switch (true) {
+                        // Cambia el tamaño del texto al 5 de bootstrap al decir "tamaño 5"
+                        case result.includes("enciende") && result.includes("luz") && result.includes("cuarto"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Encender luz del cuarto");
+                            putJson(1, 1, 0, 0, 0, 0, 0, 0);
+                            postJson("Encender luz del cuarto");
+                            break;
 
-                    case result.includes("apaga") && result.includes("luz") && result.includes("cuarto"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Apagar luz del cuarto");
-                        putJson(0,1,0,0,0,0,0,0);
-                        postJson("Apagar luz del cuarto");
-                        break;
-                       
-                    case result.includes("enciende") && result.includes("luz") && result.includes("sala"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Encender luz de la sala");
-                        putJson(1,0,1,0,0,0,0,0);
-                        postJson("Encender luz de la sala");
-                        break;
+                        case result.includes("apaga") && result.includes("luz") && result.includes("cuarto"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Apagar luz del cuarto");
+                            putJson(0, 1, 0, 0, 0, 0, 0, 0);
+                            postJson("Apagar luz del cuarto");
+                            break;
 
-                    case result.includes("apaga") && result.includes("luz") && result.includes("sala"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Apagar luz de la sala");
-                        putJson(0,0,1,0,0,0,0,0);
-                        postJson("Apagar luz de la sala");
-                        break;   
+                        case result.includes("enciende") && result.includes("luz") && result.includes("sala"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Encender luz de la sala");
+                            putJson(1, 0, 1, 0, 0, 0, 0, 0);
+                            postJson("Encender luz de la sala");
+                            break;
 
-                    case result.includes("enciende") && result.includes("luces") && result.includes("jardín"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Encender luces del jardín");
-                        putJson(1,0,0,1,0,0,0,0);
-                        postJson("Encender luces del jardín");
-                        break;
+                        case result.includes("apaga") && result.includes("luz") && result.includes("sala"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Apagar luz de la sala");
+                            putJson(0, 0, 1, 0, 0, 0, 0, 0);
+                            postJson("Apagar luz de la sala");
+                            break;
 
-                    case result.includes("apaga") && result.includes("luces") && result.includes("jardín"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Apagar luces del jardín");
-                        putJson(0,0,0,1,0,0,0,0);
-                        postJson("Apagar luces del jardín");
-                        break;   
+                        case result.includes("enciende") && result.includes("luces") && result.includes("jardín"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Encender luces del jardín");
+                            putJson(1, 0, 0, 1, 0, 0, 0, 0);
+                            postJson("Encender luces del jardín");
+                            break;
 
-                    case result.includes("enciende") && result.includes("ventilador"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Encender ventilador");
-                        putJson(1,0,0,0,1,0,0,0);
-                        postJson("Encender ventilador");
-                        break;
+                        case result.includes("apaga") && result.includes("luces") && result.includes("jardín"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Apagar luces del jardín");
+                            putJson(0, 0, 0, 1, 0, 0, 0, 0);
+                            postJson("Apagar luces del jardín");
+                            break;
 
-                    case result.includes("apaga") && result.includes("ventilador"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Apagar ventilador");
-                        putJson(0,0,0,0,1,0,0,0);
-                        postJson("Apagar ventilador");
-                        break;  
+                        case result.includes("enciende") && result.includes("ventilador"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Encender ventilador");
+                            putJson(1, 0, 0, 0, 1, 0, 0, 0);
+                            postJson("Encender ventilador");
+                            break;
 
-                    case result.includes("abre") && result.includes("cortinas"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Abrir cortinas");
-                        putJson(1,0,0,0,0,1,0,0);
-                        postJson("Abrir cortinas");
-                        break;
+                        case result.includes("apaga") && result.includes("ventilador"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Apagar ventilador");
+                            putJson(0, 0, 0, 0, 1, 0, 0, 0);
+                            postJson("Apagar ventilador");
+                            break;
 
-                    case result.includes("cierra") && result.includes("cortinas"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Cerrar Cortinas");
-                        putJson(0,0,0,0,0,1,0,0);
-                        postJson("Cerrar cortinas");
-                        break; 
+                        case result.includes("abre") && result.includes("cortinas"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Abrir cortinas");
+                            putJson(1, 0, 0, 0, 0, 1, 0, 0);
+                            postJson("Abrir cortinas");
+                            break;
 
-                    case result.includes("alarma") && result.includes("desactiva"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Desactivar alarma");
-                        putJson(0,0,0,0,0,0,1,0);
-                        postJson("Desactivar alarma");
-                        break; 
+                        case result.includes("cierra") && result.includes("cortinas"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Cerrar Cortinas");
+                            putJson(0, 0, 0, 0, 0, 1, 0, 0);
+                            postJson("Cerrar cortinas");
+                            break;
 
-                    case result.match("activa") && result.includes("alarma"):
-                        orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
-                        console.log("Activar alarma");
-                        putJson(1,0,0,0,0,0,1,0);
-                        postJson("Activar alarma");
-                        break; 
+                        case result.includes("alarma") && result.includes("desactiva"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Desactivar alarma");
+                            putJson(0, 0, 0, 0, 0, 0, 1, 0);
+                            postJson("Desactivar alarma");
+                            break;
 
-                    
-                        
-                        
+                        case result.match("activa") && result.includes("alarma"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Activar alarma");
+                            putJson(1, 0, 0, 0, 0, 0, 1, 0);
+                            postJson("Activar alarma");
+                            break;
 
+                        case result.includes("apaga") && result.includes("cámaras"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Apagar cámaras");
+                            putJson(0, 0, 0, 0, 0, 0, 0, 1);
+                            postJson("Apagar cámaras");
+                            break;
 
-                    default:
-                        orderResultDiv.innerHTML = `<p>Orden desconocida, intenta de nuevo</p>`;
-                        break;
+                        case result.includes("enciende") && result.includes("cámaras"):
+                            orderResultDiv.innerHTML = `<p>Orden identificada: <strong>${result}</strong></p>`;
+                            console.log("Encender cámaras");
+                            putJson(1, 0, 0, 0, 0, 0, 0, 1);
+                            postJson("Encender cámaras");
+                            break;
+
+                        default:
+                            orderResultDiv.innerHTML = `<p>Orden desconocida, intenta de nuevo</p>`;
+                            break;
+                    }
                 }
             }
-
-            }
-            ;
-
             // Iniciar reconocimiento
             recognition.start();
         } else {
@@ -144,7 +150,7 @@ document.addEventListener('DOMContentLoaded', function () {
     }, 10000);
 
     // Funcion para guarda ordenes o acciones realizadas de acuerdo a la accion definida en el parametro
-    function putJson(status,room,livingRoom,garden,fan,curtain,alarm,camera) {
+    function putJson(status, room, livingRoom, garden, fan, curtain, alarm, camera) {
         // La funcion trabaja con promesas para asegurar que los datos se inserten antes de que se realice la acción ya que si no
         // al cerrar una ventana esto ocurrira antes de que se envien los datos a MockApi
         return new Promise((resolve, reject) => {
@@ -158,25 +164,25 @@ document.addEventListener('DOMContentLoaded', function () {
                 { nombre: "alarm", variable: alarm },
                 { nombre: "camera", variable: camera }
             ];
-            
+
             let recurso;
-            
+
             for (let i = 0; i < recursos.length; i++) {
                 if (recursos[i].variable === 1) {
                     recurso = { [recursos[i].nombre]: status };
                     break;
                 }
             }
-            
+
             if (!recurso) {
                 console.log("No se eligió un recurso válido");
             }
-            
+
 
 
 
             // Se crea un objeto que almacena la fecha obtenida y la accion del parametro
-            
+
 
             // Se confierte el objeto a JSON
             const recursoJSON = JSON.stringify(recurso);
